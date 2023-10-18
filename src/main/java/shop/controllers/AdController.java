19ad20017b120bca43f3d2205d21dab714629785
ddragoev <ddragoev@codix.bg>
2023-10-18 21:21:26 +0300

    func changes: added a connection to a database on localhost
    tech changes:1.used application.properties for the database connection. Created a new class DatabaseConfig for the database connection using data from application.properties.
    2.Added a folder exceptions where will be the new created manually classes about exceptions.
    
    Dependencies: added dependencies in the pom.xml about unit tests and jdbc connections

--- src/main/java/shop/controllers/AdController.java
+++ src/main/java/shop/controllers/AdController.java
@@ -2,16 +2,28 @@ package shop.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import shop.dto.AdDto;
+import shop.exceptions.UnauthorizedException;
 
 
 @RestController
 public class AdController {
 
     @GetMapping("/ads/{id}")
-   public AdDto getAd(@PathVariable long id){
+    public AdDto getAd(@PathVariable long id) {
+        // Check if the user is logged in
+        if (!isLoggedIn()) {
+            throw new UnauthorizedException("User is not logged in");
+        }
         return new AdDto();
+    }
+
+    private boolean isLoggedIn() {
+        // Implement the logic to check if the user is logged in
+        // Return true if the user is logged in, otherwise return false
+        // Example implementation:
+        // return authenticationService.isUserLoggedIn();
+        return true;
     }
 }
