ce4d8a651db8732f7b7cdf90876923e43b2a53c7
ddragoev <ddragoev@codix.bg>
2023-12-15 00:25:46 +0200

    func changes: the classes from the package "model" have been created as tables in the database and the fields of the classes as columns in the created tables.
    tech changes: removed "schema-generation.scripts" properties from application.properties, so Hibernate can execute SQL about creation tables on the database but not insert them in a separate file called "create.sql"
    
    Dependencies:

--- src/main/java/shop/ShopStart.java
+++ src/main/java/shop/ShopStart.java
@@ -30,71 +30,12 @@ import java.sql.Statement;
 
 
 @SpringBootApplication
-@ComponentScan(basePackages = "shop")
-@EntityScan(basePackages = "shop.model")
-@EnableJpaRepositories(basePackages = "shop.dao")
+//@ComponentScan(basePackages = "shop")
+//@EntityScan(basePackages = "shop.model")
+//@EnableJpaRepositories(basePackages = "shop.dao")
 public class ShopStart {
     public static void main(String[] args) {
         SpringApplication.run(ShopStart.class, args);
-
-        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure("hibernate.cfg.xml")
-                .build();
-        SessionFactory factory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-        Session session = factory.openSession();
-        Transaction transaction = session.beginTransaction();
-
-        CreateUserDto newC = new CreateUserDto();
-        User newUser = new User();
-        newUser.setUserName(newC.getUserName());
-        newUser.setEmail(newC.getEmail());
-        newUser.setPassword(newC.getPassword());
-        newUser.setPhone(newC.getPhone());
-
-        session.save(newUser);
-        transaction.commit();
-        session.close();
-        factory.close();
-//
-//        MetadataSources metadataSources = new MetadataSources(serviceRegistry);
-//        metadataSources.addAnnotatedClass(User.class);
-//        metadataSources.addAnnotatedClass(Ad.class);
-//        metadataSources.addAnnotatedClass(Message.class);
-//        metadataSources.addAnnotatedClass(Photo.class);
-//
-//        Metadata metadata = metadataSources.buildMetadata();
-//
-//        SchemaCreatorImpl schemaCreator = new SchemaCreatorImpl(serviceRegistry);
-////
-//        // Use the schemaCreator to create tables
-//        schemaCreator.doCreation(metadata, true);
-//        StandardServiceRegistryBuilder.destroy(serviceRegistry);
-
-        // Database connection details
-//        String url = "jdbc:mysql://localhost:3306/shop";
-//        String username = "root";
-//        String password = "root";
-//
-//        // SQL statement to create the table
-//        String createTableSql = "    create table `users` (\n" +
-//                "        `id` bigint not null auto_increment,\n" +
-//                "        `email` varchar(255),\n" +
-//                "        `password` varchar(255),\n" +
-//                "        `phone` varchar(255),\n" +
-//                "        `user_name` varchar(255),\n" +
-//                "        primary key (`id`)\n" +
-//                "    ) engine=InnoDB;";
-//
-//        try (Connection connection = DriverManager.getConnection(url, username, password);
-//             Statement statement = connection.createStatement()) {
-//
-//            // Execute the create table statement
-//            statement.executeUpdate(createTableSql);
-//            System.out.println("Table created successfully!");
-//
-//        } catch (SQLException e) {
-//            System.out.println("Error creating table: " + e.getMessage());
-//        }
 
     }
 }
